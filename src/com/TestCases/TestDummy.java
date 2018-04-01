77be113806ecb83aaa7f63c5b209334aa10ed09d
GayiArun <37980358+GayiArun@users.noreply.github.com>
2018-04-01 22:16:00 +0530

    Update TestDummy.java

--- src/com/TestCases/TestDummy.java
+++ src/com/TestCases/TestDummy.java
@@ -6,6 +6,7 @@ import com.Utils.BaseTest;
 import com.Utils.CommonFunctions;
 
 public class TestDummy extends BaseTest {
+	int x=10;
 	public static void main(String[] args) {
 		
 		CommonFunctions.datepick("12/12/2018", drv, "//span[@id='nm']", "//td[@class='cdate' and text()='", "']");
